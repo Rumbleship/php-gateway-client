5ed8cd77e03c98bb0d3dddf50c7a705e99c4a53c
Alex Lockwood <alex.lockwood@gmail.com>
2017-09-19 12:22:07 -0700

    chore: `composer test` setup

---
+++ tests/HelloTest.php
@@ -0,0 +1,12 @@
+<?php
+use PHPUnit\Framework\TestCase;
+use Rumbleship\Hello;
+
+class HelloTest extends TestCase {
+  public function testHello() {
+    $hello = new Hello('Alex');
+    $this->assertEquals('Hello Alex.', $hello->words());
+  }
+}
+
+?>
