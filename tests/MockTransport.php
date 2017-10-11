304f2436307c091cca6f6c2bb49c01c737af6da1
Alexander Lockwood <alockwood05@alexanders-mbp.lan>
2017-10-10 17:46:52 -0700

    chore: Move mock transport class to its own file

--- tests/MockTransport.php
+++ tests/MockTransport.php
@@ -90,16 +90,4 @@ class MockTransport implements \Requests_Transport {
 	}
 }
 
-class RequestToBodyMockTransport extends MockTransport {
-  public function request($url, $headers = array(), $data = array(), $options = array()) {
-    $body_data = array(
-      'headers'=> $headers,
-      'url'=> $url,
-      'request_payload'=> $data,
-      'options'=> $options
-    );
-    $this->body = json_encode($body_data);
-    return parent::request($url, $headers = array(), $data = array(), $options = array());
-  }
-}
 
